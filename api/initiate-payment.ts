bc8053a97912e997b1b9fa7ef5fc115a1e51365b
mfkayan044 <mfkayan044@gmail.com>
2025-10-13 13:24:58 +0300

    Update import path for QNB payment module

--- api/initiate-payment.ts
+++ api/initiate-payment.ts
@@ -1,4 +1,4 @@
-import { initiate3DPayment, QNBPaymentRequest } from './qnb-payment';
+import { initiate3DPayment, QNBPaymentRequest } from './qnb-payment.js';
 
 export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') {
