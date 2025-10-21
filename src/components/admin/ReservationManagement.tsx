65f9141b567c47cf13c53f06f3c0bc527b0ff2ea
mfkayan044 <mfkayan044@gmail.com>
2025-10-21 21:08:23 +0300

    Update ReservationManagement.tsx

--- src/components/admin/ReservationManagement.tsx
+++ src/components/admin/ReservationManagement.tsx
@@ -89,7 +89,7 @@ const downloadVoucherPdf = async (reservation: any) => {
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = voucher_${reservation.reservation_number || reservation.id}.pdf;
+  a.download = `voucher_${reservation.reservation_number || reservation.id}.pdf`;
     document.body.appendChild(a);
     a.click();
     a.remove();
